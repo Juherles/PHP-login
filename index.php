59d39dfc721dd2145bfef74fb929c0822ab3e6cd
GABO0225 <juhe2512@gmail.com>
2025-04-30 19:31:46 -0500

    Cambio

---
+++ index.php
@@ -0,0 +1,23 @@
+<html>
+
+    <head>
+
+        <title> Login / Sing up PHP</title>
+
+        <!--Hola--->
+        <link rel= "stylesheet" href="CSS/style.css">
+
+    </head>
+
+    <body>
+        <h1>   Bienvenido, por favor ingresa   </h1>
+
+        <div>
+            <a href="login.php"> Acceda aqui</a> 
+            <a href="signup.php"> Registrate aqui</a> 
+        
+        </div>
+    
+    </body>
+
+</html>
